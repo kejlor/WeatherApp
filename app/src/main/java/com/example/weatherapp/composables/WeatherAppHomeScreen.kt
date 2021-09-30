7c8071fdab611c938930c61565378e341e80bbef
kejlor <woj450@gmail.com>
2021-09-30 14:36:30 +0200

    Deleted hard coded strings. Added strings in correct file. Added translation to polish.

--- app/src/main/java/com/example/weatherapp/composables/WeatherAppHomeScreen.kt
+++ app/src/main/java/com/example/weatherapp/composables/WeatherAppHomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.R
 import com.example.weatherapp.data.Weather
@@ -40,12 +42,17 @@ fun WeatherAppHomeScreen(weatherViewModel: WeatherViewModel) {
                 } else {
                     Icon(painter = painterResource(id = R.drawable._01lighticons_02), contentDescription = "", tint = Color.Yellow)
                 }
-                Text(text = "Checking weather for " + weather.name + ":")
-                Text(text = "Temperature: " + weather.main.temp.toString() + "\u00B0")
-                Text(text = "Sky: " + weather.weather[0].description)
-                Text(text = "Max temperature: " + weather.main.tempMax.toString() + "\u00B0")
-                Text(text = "Min temperature: " + weather.main.tempMin.toString() + "\u00B0")
-                Text(text = "Wind speed: " + weather.wind.speed.toString() + " km/h")
+                Text(text = stringResource(id = R.string.checking_weather_for) + weather.name + ":")
+                Divider()
+                Text(text = stringResource(id = R.string.temperature) + weather.main.temp.toString() + "\u00B0")
+                Divider()
+                Text(text = stringResource(id = R.string.sky) + weather.weather[0].description)
+                Divider()
+                Text(text = stringResource(id = R.string.max_temp) + weather.main.tempMax.toString() + "\u00B0")
+                Divider()
+                Text(text = stringResource(id = R.string.min_temp) + weather.main.tempMin.toString() + "\u00B0")
+                Divider()
+                Text(text = stringResource(id = R.string.wind_speed) + weather.wind.speed.toString() + " km/h")
             }
         }
     }
